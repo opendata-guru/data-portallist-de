c99e10c11d1dae4c72706da4218bb9a3f7a84375
Thomas Tursics <thomas@tursics.de>
2021-05-28 22:54:18 +0200

    add the gemeinde geojson file

--- daily.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-today=`date +%Y-%m-%d`
-
-curl https://ckan.govdata.de/catalog.rdf -o "downloads/de-rdf/govdata-$today.xml"
-curl https://ckan.govdata.de/api/3/action/package_list -o "downloads/de/govdata-$today.json"
-curl https://www.data.gv.at/katalog/api/3/action/package_list -o "downloads/at/data-gv-$today.json"
-curl http://data.opendataportal.at/api/3/action/package_list -o "downloads/at-odp/opendataportal-$today.json"
-curl https://opendata.swiss/api/3/action/package_list -o "downloads/ch/swiss-$today.json"
